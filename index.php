2ebea9dbede2d9d8e4d32f4baa07d47ebba05e6f
georgeGalin <papezoor.gamer@gmail.com>
2020-01-07 20:16:03 +0100

    sec

---
+++ index.php
@@ -0,0 +1,15 @@
+<HTML>
+<BODY>
+<h1>Sinonimi e contrari</h1>
+
+<FORM ACTION="mod.php"  METHOD="POST">
+<INPUT TYPE="Text" NAME="p"> Inserisci Parola <BR>
+
+
+<INPUT TYPE="Submit" VALUE="invia">
+<INPUT TYPE="Reset" VALUE="cancella">
+
+</FORM>
+
+</BODY>
+</HTML>
---
+++ mod.php
@@ -0,0 +1,20 @@
+<HTML>
+<BODY>
+
+<?php
+     
+$p=$_POST['p'];
+
+    if($p=="bravo"){
+        echo"$p<br><br><br>SINONIMi= buono, corretto, fidato, generoso, leale, onesto, probo, [posposto] ammodo <br> CONTRARI:attivo, malvagio, immorale, disonesto";
+    }
+    if($p=="amabile"){
+        echo"$p<br><br><br>SINONIMI= con gentilezza, con dolcezza, graziosamente <br> CONTRARI= odioso, antipatico, scortese";
+        }
+    if($p=="babbeo"){
+        echo"$p<br><br><br>SINONIMI= sciocco, sempliciotto, ingenuo, stolto, buono a nulla <br>CONTRARI= furbo, scaltro, astuto, intelligente, vivace";
+            }
+
+?>
+</BODY>
+</HTML>
